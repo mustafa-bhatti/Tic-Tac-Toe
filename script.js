5220d065bed8465a3be93d7af811f8015e9477cf
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-06-27 03:44:19 +0500

    add the checkWin method for both players

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gameboard = (function(){
     let board = [];
-    let player1,player2;
+    let player1 = "X";
+    let player2 = "O"
 
     const init = function(){
         for(let i=0;i<9;i++){
@@ -12,28 +13,51 @@ const gameboard = (function(){
         console.log(board.slice(0,3))
         console.log(board.slice(3,6))
         console.log(board.slice(6,9))
-
-
     }
 
     const move = function(pos,player){
         if (player == "player1"){
-            player = "X"
+            player = player1
         }
         else {
-            player = "O"
+            player = player2
         }
         if (typeof(board[pos]) == "number"){
             board[pos] = player;
         }
-        else {
-         alert("ERROR: already a marker there")}
+        // else {
+        //  alert("ERROR: already a marker there")}
         console.log(`new move at index: ${pos}`)
         displayBoard()
-    }
+        checkWin(player1)
+        checkWin(player2)
 
+
+    }
+    const checkWin = function(marker){
+        let winCombos = [
+            [0,1,2],[3,4,5],[6,7,8],
+            [0,3,6],[1,4,7],[2,5,8],
+            [0,4,8], [2,4,6]
+        ]
+
+        for (let i =0;i<winCombos.length;i++){
+            let win = true;
+            for (let x of winCombos[i]){
+                if (board[x] != marker){
+                    win = false;
+                    break}
+            } 
+            if (win == true){
+                console.log("Player ",marker," Wins")
+                return
+            }
+        }
+    }
+    
     init()
     displayBoard()
+
     return {
         init,
         displayBoard,
@@ -41,8 +65,14 @@ const gameboard = (function(){
     }
 })()
 
+gameboard.move(2,"player1")
 gameboard.move(4,"player1")
-gameboard.move(2,"O")
+gameboard.move(6,"player1")
+gameboard.move(0,"player2")
+gameboard.move(1,"player2")
+
+
+
 
 
 
