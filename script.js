ed0a9392393526fe0ba5f36775a988dcab22e106
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-06-27 02:47:32 +0500

    add move method that takes in position and player marker

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const gameboard = (function(){
     let board = [];
+    let player1,player2;
 
     const init = function(){
         for(let i=0;i<9;i++){
-            board[i] = 0
+            board[i] = i
         }
         // console.log(board)
     }
@@ -12,10 +13,36 @@ const gameboard = (function(){
         console.log(board.slice(3,6))
         console.log(board.slice(6,9))
 
+
     }
+
+    const move = function(pos,player){
+        if (player == "player1"){
+            player = "X"
+        }
+        else {
+            player = "O"
+        }
+        if (typeof(board[pos]) == "number"){
+            board[pos] = player;
+        }
+        else {
+         alert("ERROR: already a marker there")}
+        console.log(`new move at index: ${pos}`)
+        displayBoard()
+    }
+
     init()
     displayBoard()
     return {
-        init
+        init,
+        displayBoard,
+        move
     }
 })()
+
+gameboard.move(4,"player1")
+gameboard.move(2,"O")
+
+
+
